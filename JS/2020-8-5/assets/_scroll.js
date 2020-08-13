cd98d153e0c345d4ea6544760a3d9b7810045295
Unidentified_Lin <jp810412@gmail.com>
2020-08-13 16:48:44 +0800

    RWD scroll and layout fix.

--- JS/2020-8-5/assets/_scroll.js
+++ JS/2020-8-5/assets/_scroll.js
@@ -39,19 +39,13 @@ export function initSlick() {
 			{
 				breakpoint: 768,
 				settings: {
-					arrows: false,
-					centerMode: true,
 					centerPadding: "10px",
-					slidesToShow: 3,
 				},
 			},
 			{
 				breakpoint: 480,
 				settings: {
-					arrows: false,
-					centerMode: true,
-					centerPadding: "5px",
-					slidesToShow: 1,
+					centerPadding: "0px",
 				},
 			},
 		],
